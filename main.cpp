1be1ec965ec31fd4ac7aabe60a82fa0c0bc4a800
LeAnne B <branchleannelmb@gmail.com>
2024-03-13 09:50:10 -0400

    updates game background color to darkblue

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main () {
 
+    Color darkBlue = {44, 44, 127, 255};
     InitWindow(300, 600, "Tetris");
     SetTargetFPS(60);
    
    while(WindowShouldClose() == false) {
 
         BeginDrawing();
+        ClearBackground(darkBlue);
 
         EndDrawing();
    }
