c26d6813ec2cc9d9fc9af043fae4c42f75886420
LeAnne B <branchleannelmb@gmail.com>
2024-03-22 10:17:21 -0400

    restructures code into Game class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <raylib.h>
-#include "grid.h"
-#include "blocks.cpp"
+// #include "grid.h"
+// #include "blocks.cpp"
+#include "game.h"
 
 using namespace std;
 
@@ -11,22 +12,25 @@ int main () {
     InitWindow(300, 600, "Tetris");
     SetTargetFPS(60);
 
-    Grid grid = Grid();
+    Game game = Game();
+
+    //Grid grid = Grid();
     // grid.grid[0][0] = 1;
     // grid.grid[3][5] = 4;
     // grid.grid[17][8] = 7;
-    grid.Print();
+    //grid.Print();
 
     //LBlock block = LBlock(); 
-    TBlock block = TBlock();
+    //TBlock block = TBlock();
     //block.Move(4,3); 
    
    while(WindowShouldClose() == false) {
 
         BeginDrawing();
         ClearBackground(darkBlue);
-        grid.Draw();
-        block.Draw();
+        //grid.Draw();
+        //block.Draw();
+        game.Draw();
 
         EndDrawing();
    }
